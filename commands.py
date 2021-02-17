77596eb6eb30723df9f2912a05f773c4f78c6f90
Andrew Nguyen <andrewnguyenn229@gmail.com>
2021-02-17 22:57:18 +1000

    Added another command to list of commands

--- commands.py
+++
@@ -1,5 +0,0 @@
-cmds = ['$pyjokes - random programming joke',
-    '$hello - get a greeting',
-    '$goodbye - say goodbye to portra :(',
-    '$dice - roll a 6 sided die'
-    '$rng {start} {end} - get a random number (integer) between {start} and {end} inclusive']
