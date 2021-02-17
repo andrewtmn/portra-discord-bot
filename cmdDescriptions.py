fb21c8883b6b38881e26aefc6f0fe393504f0834
Andrew Nguyen <andrewnguyenn229@gmail.com>
2021-02-17 22:57:58 +1000

    renamed commands.py to cmdDescriptions.py (discord api already has a commands module)

---
+++ cmdDescriptions.py
@@ -0,0 +1,8 @@
+cmds = [
+    '$meme - random meme in image format',
+    '$pyjoke - random programming joke',
+    '$hello - get a greeting',
+    '$goodbye - say goodbye to portra :(',
+    '$dice {sides} - roll a {sides}-sided die',
+    '$rng {start} {end} - get a random number (integer) between {start} and {end} inclusive'
+    ]
