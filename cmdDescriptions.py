b32b413acada80326450c81dc622210a6d457068
Andrew Nguyen <andrewnguyenn229@gmail.com>
2021-02-22 10:34:29 +1000

    Added the music commands to the list of commands that will be printed when  is called

--- cmdDescriptions.py
+++ cmdDescriptions.py
@@ -1,4 +1,9 @@
 cmds = [
+    '$play {url} - play the audio from the youtube video given by {url}',
+    '$pause - pause the current youtube audio being played',
+    '$resume - resume the youtube audio that has been paused',
+    '$stop - stop the current youtube audio being played',
+    '$leave - Portra will leave the voice channel it is currently in',
     '$meme - random meme in image format',
     '$pyjoke - random programming joke',
     '$hello - get a greeting',
